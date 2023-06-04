367c555e4bb50b2687e74b01b4136cd6edd5923a
Iz <ido23j@gmail.com>
2023-06-04 18:58:50 +0900

    #6 effects

--- src/App.js
+++ src/App.js
@@ -1,54 +1,43 @@
-import styles from "./App.module.css";
-import { useEffect, useState } from "react";
-
-function Hello() {
-  // function byeFn() {
-  //   console.log("bye ");
-  // }
-  // function hiFn() {
-  //   console.log("hi");
-  //   return byeFn;
-  // }
-  // useEffect(hiFn, []);
-  useEffect(() => {
-    console.log("creating Hello");
-    return () => console.log("destroying Hello");
-  }, []);
-  return <h1>Hello</h1>;
-}
+import { useState } from "react";
 
 function App() {
-	const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const [showing, setShowing] = useState(true);
-	const onClick = () => setCounter((prev) => prev + 1);
-	const onChange = (event) => {
-		setKeyword(event.target.value);
+	const [toDo, setToDo] = useState([]);
+	const [toDos, setToDos] = useState([]);
+	const onChange = (event) => setToDo(event.target.value);
+	const onSubmit = (event) => {
+		event.preventDefault();
+		if (toDo === "") {
+			return;
+		}
+		const toDoObj = {
+			text: toDo,
+			id: Date.now(),
+		};
+		setToDos((currentArray) => [...currentArray, toDoObj]);
+		setToDo("");
 	};
-	// watch the keyword
-  useEffect(() => {
-    if (keyword === "" || keyword.length < 5) return;
-		console.log("searching for", keyword);
-  }, [keyword]);
-  // watch the counter
-  useEffect(() => {
-		console.log(" counter ",  counter);
-  }, [counter]);
-  
 	return (
-    <div className="App">
-      <button onClick={() => setShowing((prev) => !prev)}>Toggle</button>
-      {showing ? <Hello /> : null }
-			<input
-				onChange={onChange}
-				type="text"
-				value={keyword}
-				placeholder="search here..."
-			/>
-			<h1 className={styles.title}>{counter}</h1>
-			<button onClick={onClick} >Click me</button>
+		<div className="App">
+			<h1> hello</h1>
+			<form onSubmit={onSubmit}>
+				<input
+					value={toDo}
+					onChange={onChange}
+					type="text"
+					placeholder="write your todo"
+				/>
+				<button type="submit">Add</button>
+			</form>
+			<hr />
+			<div>
+				<h1>Todo List</h1>
+				<ul>
+					{toDos.map((toDoObj, index) => (
+						<li key={toDoObj.id}>{toDoObj.text}</li>
+					))}
+				</ul>
+			</div>
 		</div>
 	);
 }
-
 export default App;
